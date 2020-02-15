7d7cc4c1378d31b51fc2ea10fca09798fd2aa99d
Martin Gonzalez Lopez <mrtglezlpz@gmail.com>
2020-02-16 00:14:05 +0100

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
       <div className="contenedor_blanco">
         <a href="https://institucional.us.es/cprevias/index.php" title="inicio">
           <img
-            src="./Cita Previa __ Gesti�n de Citas On-Line de la Universidad de Sevilla_files/cabecera-left.gif"
+            src="./Cita Previa __ Gestión de Citas On-Line de la Universidad de Sevilla_files/cabecera-left.gif"
             alt="Cita Previa"
             id="cabecera-left"
           />
           <img
-            src="./Cita Previa __ Gesti�n de Citas On-Line de la Universidad de Sevilla_files/cabecera-right.gif"
+            src="./Cita Previa __ Gestión de Citas On-Line de la Universidad de Sevilla_files/cabecera-right.gif"
             alt="Cita Previa"
             id="cabecera-right"
           />
@@ -37,9 +37,9 @@ function App() {
           {/* ///style="text-align:right;margin-bottom:-23px;" */}
           <a
             href="https://institucional.us.es/cprevias/salir.php"
-            title="Cerrar la sesi�n"
+            title="Cerrar la sesión"
           >
-            Cerrar sesi�n
+            Cerrar sesión
           </a>
           &nbsp;
         </p>{" "}
@@ -55,7 +55,7 @@ function App() {
           <fieldset id="fieldsetCitaPrevia" className="fieldsetCitaPrevia">
             <h1>1. Centro</h1>
             <label className="labelCitaPrevia2">
-              Escuela T�cnica Superior de Ingenier�a Inform�tica
+              Escuela Tócnica Superior de Ingenieróa Informótica
             </label>
             <br /> <input type="hidden" name="paso" value="3" />
             <h1>2. Selecciona un Servicio</h1>
@@ -66,67 +66,67 @@ function App() {
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="116" />
-                Presentaci�n de otras solicitudes
+                Presentación de otras solicitudes
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="119" />
-                Presentaci�n de solicitud de devoluci�n
+                Presentación de solicitud de devolución
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="115" />
-                Presentaci�n solicitud cambio Departamento Proyecto
+                Presentación solicitud cambio Departamento Proyecto
               </div>
             </div>
             <div id="contenedora">
               <div className="capaTipoServicio">
                 <br />
-                <b>Ampliaci�n de matr�cula</b>
+                <b>Ampliación de matrócula</b>
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="112" />
-                Presentaci�n de solicitud de ampliaci�n de matr�cula
+                Presentación de solicitud de ampliación de matrócula
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="113" />
-                Recogida carta de pago ampliaci�n matr�cula
+                Recogida carta de pago ampliación matrócula
               </div>
             </div>
             <div id="contenedora">
               <div className="capaTipoServicio">
                 <br />
-                <b>Pr�cticas en Empresas</b>
+                <b>Prócticas en Empresas</b>
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="100" />
-                Presentaci�n de solicitud de pr�cticas
+                Presentación de solicitud de prócticas
               </div>
             </div>
             <div id="contenedora">
               <div className="capaTipoServicio">
                 <br />
-                <b>Informaci�n </b>
+                <b>Información </b>
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="90" />
-                Informaci�n general
+                Información general
               </div>
             </div>
             <div id="contenedora">
               <div className="capaTipoServicio">
                 <br />
-                <b>Reconocimientos de cr�ditos</b>
+                <b>Reconocimientos de cróditos</b>
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="92" />
-                Entrega de documentaci�n de reconocimiento de cr�ditos
+                Entrega de documentación de reconocimiento de cróditos
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="96" />
-                Informaci�n sobre reconocimientos y adaptaciones
+                Información sobre reconocimientos y adaptaciones
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="91" />
-                Presentaci�n de solicitud de reconocimiento de cr�ditos
+                Presentación de solicitud de reconocimiento de cróditos
               </div>
             </div>
             <div id="contenedora">
@@ -150,59 +150,59 @@ function App() {
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="98" />
-                Presentaci�n de solicitud de traslado de expediente
+                Presentación de solicitud de traslado de expediente
               </div>
             </div>
             <div id="contenedora">
               <div className="capaTipoServicio">
                 <br />
-                <b>T�tulos</b>
+                <b>Tótulos</b>
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="118" />
-                Presentaci�n de solicitud de t�tulo
+                Presentación de solicitud de tótulo
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="103" />
-                Recogida del resguardo de T�tulo
+                Recogida del resguardo de Tótulo
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="26" />
-                Recogida del t�tulo
+                Recogida del tótulo
               </div>
             </div>
             <div id="contenedora">
               <div className="capaTipoServicio">
                 <br />
-                <b>Matriculaci�n</b>
+                <b>Matriculación</b>
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="138" />
-                Compulsa y revisi�n de matr�culas
+                Compulsa y revisión de matróculas
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="95" />
-                Informaci�n sobre matr�cula
+                Información sobre matrócula
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="108" />
-                Presentaci�n de solicitud de anulaci�n de matr�cula
+                Presentación de solicitud de anulación de matrócula
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="110" />
-                Presentaci�n de solicitud de cambio de grupo ingl�s
+                Presentación de solicitud de cambio de grupo inglós
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="109" />
-                Presentaci�n de solicitud de modificaci�n de matr�cula
+                Presentación de solicitud de modificación de matrócula
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="111" />
-                Presentaci�n de solicitud de permuta
+                Presentación de solicitud de permuta
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="107" />
-                Presentaci�n solicitud matr�cula Pr�cticas en Empresas
+                Presentación solicitud matrócula Prócticas en Empresas
               </div>
             </div>
             <div id="contenedora">
@@ -212,11 +212,11 @@ function App() {
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="7" />
-                Recogida de Certificaciones Acad�micas
+                Recogida de Certificaciones Acadómicas
               </div>
               <div className="capaServicio">
                 <input type="radio" name="cs" value="94" />
-                Solicitud de certificaciones acad�micas
+                Solicitud de certificaciones acadómicas
               </div>
             </div>
             <input
@@ -232,8 +232,8 @@ function App() {
               <p>
                 <br />
                 <br />
-                (Recuerda que s�lo puedes pedir 1 Cita Previa para el mismo
-                Centro - Servicio - D�a)
+                (Recuerda que sólo puedes pedir 1 Cita Previa para el mismo
+                Centro - Servicio - Dóa)
               </p>{" "}
               <input
                 type="submit"
@@ -253,7 +253,7 @@ function App() {
         <p>
           <b>
             Universidad de Sevilla. C/ S. Fernando, 4, C.P. 41004-Sevilla,
-            Espa�a. Centralita exterior: 954551000 citapreviafefp@us.es
+            Espaóa. Centralita exterior: 954551000 citapreviafefp@us.es
           </b>
           <span className="contacto">
             <a
